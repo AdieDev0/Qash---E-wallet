3bdb1ff40bb59e11a8e00cf306c1f0afadbd5b73
AdieDev0 <devutella@gmail.com>
2025-03-01 23:07:03 +0800

    update ThemeToggle styles for improved accessibility and visual consistency

--- QASH/src/DarkMode/ThemeToggle.jsx
+++ QASH/src/DarkMode/ThemeToggle.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa";
 import { MdSunny } from "react-icons/md";
+
 const ThemeToggle = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
@@ -16,12 +17,12 @@ const ThemeToggle = () => {
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="fixed bottom-5 left-5 p-3 bg-white dark:bg-violet-800 border border-gray-800 dark:border-white rounded-full shadow-lg hover:scale-110 transition duration-200"
+      className="fixed bottom-5 left-5 p-3 bg-gray-200 dark:bg-gray-800 border border-gray-800 dark:border-gray-300 rounded-full shadow-lg hover:scale-110 transition duration-200"
     >
       {theme === "dark" ? (
-        <MdSunny className="text-violet-400 text-4xl" />
+        <MdSunny className="text-yellow-400 text-4xl" />
       ) : (
-        <FaMoon className="text-gray-800 text-4xl" />
+        <FaMoon className="text-gray-800 dark:text-white text-4xl" />
       )}
     </button>
   );
