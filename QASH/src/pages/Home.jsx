c9746c084449767a4f54337a06c95ff6823a22ae
AdieDev0 <devutella@gmail.com>
2025-03-01 22:03:50 +0800

    add CreditCards image to Home component and update layout

--- QASH/src/pages/Home.jsx
+++ QASH/src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import CreditCards from "../assets/Home/CreditCards.jpg";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div>
+        <div className="relative">
+          <img
+            src={CreditCards}
+            alt=""
+            className="w-full object-cover h-[500px]"
+          />
+          <div className="absolute inset-0 bg-black opacity-80"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
