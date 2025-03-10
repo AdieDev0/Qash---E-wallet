ea839494daed78f2ff23f209e0ab9124a7846284
AdieDev0 <devutella@gmail.com>
2025-03-10 12:50:49 +0800

    fix: correct import path for Navbar component in App.jsx

--- QASH/src/App.jsx
+++ QASH/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/navbar";
+import Navbar from "./Components/Navbaravbar";
 import Footer from "./Components/Footer";
 import Mini from "./Components/Mini";
 import Home from "./pages/Home";
